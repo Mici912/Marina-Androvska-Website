6a1ab1d7594fc26b88e36165e6f2a23d96bbb6ac
Mici912 <marinaandrovska@gmail.com>
2025-03-03 21:32:26 +0000

    Including Video Sources

--- Java Script.js
+++ Java Script.js
@@ -83,3 +83,21 @@ document.addEventListener('DOMContentLoaded', () => {
         firefliesContainer.appendChild(firefly);
     }
 });
+
+    function openModal(modalId) {
+        document.getElementById(modalId).style.display = "block";
+    }
+
+    function closeModal(modalId) {
+        document.getElementById(modalId).style.display = "none";
+    }
+
+    // Close the modal if clicked outside of content
+    window.onclick = function (event) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach((modal) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+        });
+    };
